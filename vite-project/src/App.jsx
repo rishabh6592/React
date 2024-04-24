b3acff58e3b1270b4ea6e8fd9ccf8b11e8f68b06
rishabh6592 <129681143+rishabh6592@users.noreply.github.com>
2024-04-24 19:54:03 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,29 @@
+// const App = () => {
+//   let abc = 12;
+//   return (
+//     <div>
+//       <h1>{abc}</h1>
+//       <h1>hello</h1>
+//       <p>Lorem ipsum dolor sit amet.</p>
+//       <button>click</button>
+//     </div>
+//   );
+// };  
+
+
+
+
+const App = ()=>{
+  return(
+    <>
+    <header><h1>unknown</h1></header>
+    <section><h1>undefine</h1></section>
+    <footer><h1>stranger  </h1></footer>
+    </>
+  );
+};
+
+
+
+
+export default App;
